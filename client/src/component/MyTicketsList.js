49ea2a92ebc256540fe3c51a863ec70fd0481e50
Indra Pradipta <59110393+iprdpta@users.noreply.github.com>
2020-03-10 13:56:30 +0700

    commit

--- client/src/component/MyTicketsList.js
+++ client/src/component/MyTicketsList.js
@@ -33,138 +33,147 @@ const MyTicketsList = props => {
           <Container className="my-tickets-text">
             <label>My Tickets</label>
           </Container>
-          {ticket.map((item, index) => (
-            <Row key={index}>
-              <Col lg={2}></Col>
-              <Col lg={8}>
-                <Container fluid className="ticketLists shadow">
-                  <Row noGutters>
-                    <Col lg={8}>
-                      <Container fluid className="ticket-list-logo-container">
-                        <Image className="ticket-list-logo" src={traintick} />
-                      </Container>
-                      <Row noGutters className="ticket-list-textleft">
-                        <Col lg={4} className="trainName">
-                          <label className="ticket-list-trainName">
-                            {item.ticket && item.ticket.train_name}
-                          </label>
+          {!ticket.length < 1 ? (
+            ticket.map((item, index) => (
+              <Row key={index}>
+                <Col lg={2}></Col>
+                <Col lg={8}>
+                  <Container fluid className="ticketLists shadow">
+                    <Row noGutters>
+                      <Col lg={8}>
+                        <Container fluid className="ticket-list-logo-container">
+                          <Image className="ticket-list-logo" src={traintick} />
+                        </Container>
+                        <Row noGutters className="ticket-list-textleft">
+                          <Col lg={4} className="trainName">
+                            <label className="ticket-list-trainName">
+                              {item.ticket && item.ticket.train_name}
+                            </label>
+                            <br />
+                            <label className="ticket-list-trainType">
+                              {item.ticket && item.ticket.traintype.name}
+                            </label>
+                            {item.status === "Waiting Payment" ? (
+                              <Container bsPrefix="ticket-list-waiting">
+                                <label>{item.status}</label>
+                              </Container>
+                            ) : null}
+                            {item.status === "Pending" ? (
+                              <Container bsPrefix="ticket-list-pending">
+                                <label>{item.status}</label>
+                              </Container>
+                            ) : null}
+                            {item.status === "Approved" ? (
+                              <Container bsPrefix="ticket-list-approved">
+                                <label>{item.status}</label>
+                              </Container>
+                            ) : null}
+                            {item.status === "Declined" ? (
+                              <Container bsPrefix="ticket-list-declined">
+                                <label>{item.status}</label>
+                              </Container>
+                            ) : null}
+                          </Col>
+
+                          <Col lg={1}>
+                            <Image className="ticket-icon" src={arrow} />
+                          </Col>
+                          <Col lg={7}>
+                            <Row noGutters className="ticket-list-textleft">
+                              <Col lg={6}>
+                                <label className="ticket-list-textbolt">
+                                  {item.ticket && item.ticket.start_time}
+                                </label>
+                                <p>{item.ticket && item.ticket.start_date}</p>
+                              </Col>
+                              <Col lg={6}>
+                                <label className="ticket-list-textbolt">
+                                  {item.ticket && item.ticket.depart}
+                                </label>
+                                <p>
+                                  {item.ticket && item.ticket.depart_station}
+                                </p>
+                              </Col>
+                            </Row>
+                            <Row noGutters className="ticket-list-textleft">
+                              <Col lg={6}>
+                                <label className="ticket-list-textbolt">
+                                  {item.ticket && item.ticket.arrival_time}
+                                </label>
+                                <p>{item.ticket && item.ticket.arrival_date}</p>
+                              </Col>
+                              <Col lg={6}>
+                                <label className="ticket-list-textbolt">
+                                  {item.ticket && item.ticket.destination}
+                                </label>
+                                <p>
+                                  {item.ticket &&
+                                    item.ticket.destination_station}
+                                </p>
+                              </Col>
+                            </Row>
+                          </Col>
+                        </Row>
+                      </Col>
+                      <Col lg={4}>
+                        <Container bsPrefix="ticket-list-trainx">
+                          <label className="ticket-list-train">Train</label>
                           <br />
-                          <label className="ticket-list-trainType">
-                            {item.ticket && item.ticket.traintype.name}
+                          <label className="ticket-list-day">Saturday</label>
+                          <label className="ticket-list-date">
+                            , {item.ticket && item.ticket.start_date}
                           </label>
-                          {item.status === "Waiting Payment" ? (
-                            <Container bsPrefix="ticket-list-waiting">
-                              <label>{item.status}</label>
-                            </Container>
-                          ) : null}
-                          {item.status === "Pending" ? (
-                            <Container bsPrefix="ticket-list-pending">
-                              <label>{item.status}</label>
-                            </Container>
-                          ) : null}
-                          {item.status === "Approved" ? (
-                            <Container bsPrefix="ticket-list-approved">
-                              <label>{item.status}</label>
-                            </Container>
-                          ) : null}
-                          {item.status === "Declined" ? (
-                            <Container bsPrefix="ticket-list-declined">
-                              <label>{item.status}</label>
-                            </Container>
-                          ) : null}
-                        </Col>
+                        </Container>
+                      </Col>
+                    </Row>
 
-                        <Col lg={1}>
-                          <Image className="ticket-icon" src={arrow} />
-                        </Col>
-                        <Col lg={7}>
-                          <Row noGutters className="ticket-list-textleft">
-                            <Col lg={6}>
-                              <label className="ticket-list-textbolt">
-                                {item.ticket && item.ticket.start_time}
-                              </label>
-                              <p>{item.ticket && item.ticket.start_date}</p>
-                            </Col>
-                            <Col lg={6}>
-                              <label className="ticket-list-textbolt">
-                                {item.ticket && item.ticket.depart}
-                              </label>
-                              <p>{item.ticket && item.ticket.depart_station}</p>
-                            </Col>
-                          </Row>
-                          <Row noGutters className="ticket-list-textleft">
-                            <Col lg={6}>
-                              <label className="ticket-list-textbolt">
-                                {item.ticket && item.ticket.arrival_time}
-                              </label>
-                              <p>{item.ticket && item.ticket.arrival_date}</p>
-                            </Col>
-                            <Col lg={6}>
-                              <label className="ticket-list-textbolt">
-                                {item.ticket && item.ticket.destination}
-                              </label>
-                              <p>
-                                {item.ticket && item.ticket.destination_station}
-                              </p>
-                            </Col>
-                          </Row>
-                        </Col>
-                      </Row>
-                    </Col>
-                    <Col lg={4}>
-                      <Container bsPrefix="ticket-list-trainx">
-                        <label className="ticket-list-train">Train</label>
-                        <br />
-                        <label className="ticket-list-day">Saturday</label>
-                        <label className="ticket-list-date">
-                          , {item.ticket && item.ticket.start_date}
-                        </label>
-                      </Container>
-                    </Col>
-                  </Row>
-
-                  <Row noGutters className="ticket-customer-data">
-                    <Col lg={2}>
-                      <label>ID Number</label>
-                    </Col>
-                    <Col lg={3}>
-                      <label>Customer</label>
-                    </Col>
-                    <Col lg={2}>
-                      <label>Phone Number</label>
-                    </Col>
-                    <Col lg={3}>
-                      <label>Email</label>
-                    </Col>
-                  </Row>
-                  <Row noGutters className="ticket-customer-datax">
-                    <Col lg={2}>
-                      <label>{item.user && item.user.id_card}</label>
-                    </Col>
-                    <Col lg={3}>
-                      <label>{item.user && item.user.name}</label>
-                    </Col>
-                    <Col lg={2}>
-                      <label>{item.user && item.user.phone}</label>
-                    </Col>
-                    <Col lg={3}>
-                      <label>{item.user && item.user.email}</label>
-                    </Col>
-                    <Col lg={2}>
-                      {item.status === "Waiting Payment" ? (
-                        <Link to={`invoice/${item.id}`}>
-                          <Button bsPrefix="ticket-payment-button">
-                            Pay Now
-                          </Button>
-                        </Link>
-                      ) : null}
-                    </Col>
-                  </Row>
-                </Container>
-              </Col>
-              <Col lg={2}></Col>
-            </Row>
-          ))}
+                    <Row noGutters className="ticket-customer-data">
+                      <Col lg={2}>
+                        <label>ID Number</label>
+                      </Col>
+                      <Col lg={3}>
+                        <label>Customer</label>
+                      </Col>
+                      <Col lg={2}>
+                        <label>Phone Number</label>
+                      </Col>
+                      <Col lg={3}>
+                        <label>Email</label>
+                      </Col>
+                    </Row>
+                    <Row noGutters className="ticket-customer-datax">
+                      <Col lg={2}>
+                        <label>{item.user && item.user.id_card}</label>
+                      </Col>
+                      <Col lg={3}>
+                        <label>{item.user && item.user.name}</label>
+                      </Col>
+                      <Col lg={2}>
+                        <label>{item.user && item.user.phone}</label>
+                      </Col>
+                      <Col lg={3}>
+                        <label>{item.user && item.user.email}</label>
+                      </Col>
+                      <Col lg={2}>
+                        {item.status === "Waiting Payment" ? (
+                          <Link to={`invoice/${item.id}`}>
+                            <Button bsPrefix="ticket-payment-button">
+                              Pay Now
+                            </Button>
+                          </Link>
+                        ) : null}
+                      </Col>
+                    </Row>
+                  </Container>
+                </Col>
+                <Col lg={2}></Col>
+              </Row>
+            ))
+          ) : (
+            <label>
+              You haven't booked the ticket, please book the ticket first
+            </label>
+          )}
         </Container>
       </>
     );
